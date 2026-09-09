0ffd21f4afb8aca0885c01e1e5f5605d243d3d62
reshandytaftazani <Reshandy.taftazani@ui.ac.id>
2026-09-09 12:58:55 +0700

    Tutorial 2(tanpa perubahan)

---
+++ main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Reshandy",
+        "npm": "2506547651",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "CS student at Universitas Indonesia."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Reshandy",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
