6de69fef1a4672d91927e3e41a637e65c1ff35b1
reshandytaftazani <Reshandy.taftazani@ui.ac.id>
2026-09-14 15:59:54 +0700

    Feat(experience): Memperbaiki bagian experience, serta menambah file css tersendiri untuk bagian experience

--- main/models.py
+++ main/models.py
@@ -9,6 +9,8 @@ class Experience(models.Model):
         ('part-time', 'Part-Time'),
         ('full-time', 'Full-Time'),
         ('freelance', 'Freelance'),
+        ('organization', 'Organization'),
+        ('competition', 'Competition'),
     ]
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
